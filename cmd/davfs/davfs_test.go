dfc99a62a93a64b09cbab67418291ffacd949c01
Nikita Konev <nikit.cpp@yandex.ru>
2019-01-20 04:21:28 +0300

    introduce parametrized tests

--- cmd/davfs/davfs_test.go
+++ cmd/davfs/davfs_test.go
@@ -12,8 +12,6 @@ import (
 )
 
 const (
-	driver = "memory"
-	source = "."
 	create = false
 	body   = `
 <?xml version="1.0" encoding="utf-8"?>
@@ -28,100 +26,126 @@ const (
 	authHeader = "Basic dXNlcjpwYXNzd29yZA=="
 )
 
+type testContext struct {
+	driverName string
+	source     string
+}
+
+var drivers = []testContext{
+	{"memory", ""},
+}
+
+func runOnAllDrivers(t *testing.T, testCase func(tc testContext)) {
+	for _, tt := range drivers {
+		t.Run(tt.driverName, func(t *testing.T) {
+			testCase(tt)
+		})
+	}
+}
+
 func TestGetCredFailedWithoutCredentials(t *testing.T) {
 
-	driver, source, cred, create := driver, source, "user:password", create
-
-	handler, e := createServer(&driver, &source, &cred, &create)
-	assert.Nil(t, e)
-
-	req := test.NewRequest("PROPFIND", "/", strings.NewReader(body))
-	w := test.NewRecorder()
-	handler.ServeHTTP(w, req)
-
-	resp := w.Result()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	fmt.Println(resp.StatusCode)
-	fmt.Println(resp.Header.Get("Content-Type"))
-	fmt.Println(string(body))
-	assert.Equal(t, 401, resp.StatusCode)
-}
-
-func TestGetCredFailedBadCredentials(t *testing.T) {
-
-	driver, source, cred, create := driver, source, "user:password2", create
-
-	handler, e := createServer(&driver, &source, &cred, &create)
-	assert.Nil(t, e)
-
-	req := test.NewRequest("PROPFIND", "/", strings.NewReader(body))
-	header := map[string][]string{
-		"Authorization": {authHeader},
-	}
-	req.Header = header
-	w := test.NewRecorder()
-	handler.ServeHTTP(w, req)
-
-	resp := w.Result()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	fmt.Println(resp.StatusCode)
-	fmt.Println(resp.Header.Get("Content-Type"))
-	fmt.Println(string(body))
-	assert.Equal(t, 401, resp.StatusCode)
-}
-
-func TestGetCredSuccess(t *testing.T) {
-
-	driver, source, cred, create := driver, source, "user:password", create
-
-	handler, e := createServer(&driver, &source, &cred, &create)
-	assert.Nil(t, e)
-
-	req := test.NewRequest("PROPFIND", "/", strings.NewReader(body))
-	header := map[string][]string{
-		"Authorization": {authHeader},
-	}
-	req.Header = header
-	w := test.NewRecorder()
-	handler.ServeHTTP(w, req)
-
-	resp := w.Result()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	fmt.Println(resp.StatusCode)
-	fmt.Println(resp.Header.Get("Content-Type"))
-	fmt.Println(string(body))
-	assert.Equal(t, 207, resp.StatusCode)
-}
-
-func TestClientCreateDir(t *testing.T) {
-	for i := 0; i < 100; i++ {
-		port := "9998"
-
-		user := "user"
-		password := "password"
-
-		driver, source, cred, create, addr := driver, source, user+":"+password, create, ":"+port
+	runOnAllDrivers(t, func(tc testContext) {
+		driver, source, cred, create := tc.driverName, tc.source, "user:password", create
 
 		handler, e := createServer(&driver, &source, &cred, &create)
 		assert.Nil(t, e)
 
-		ctx, cancel := context.WithCancel(context.Background())
-		srv := runServer(&addr, handler)
+		req := test.NewRequest("PROPFIND", "/", strings.NewReader(body))
+		w := test.NewRecorder()
+		handler.ServeHTTP(w, req)
 
-		uri := "http://localhost:" + port
+		resp := w.Result()
 
-		c := gowebdav.NewClient(uri, user, password)
-		_ = c.Connect() // performs authorization
-		err := c.Mkdir("folder", 0644)
-		assert.Nil(t, err, "Got error %v", err)
+		body, _ := ioutil.ReadAll(resp.Body)
 
-		srv.Shutdown(ctx)
-		cancel()
-	}
+		fmt.Println(resp.StatusCode)
+		fmt.Println(resp.Header.Get("Content-Type"))
+		fmt.Println(string(body))
+		assert.Equal(t, 401, resp.StatusCode)
+	})
+
+}
+
+func TestGetCredFailedBadCredentials(t *testing.T) {
+	runOnAllDrivers(t, func(tc testContext) {
+
+		driver, source, cred, create := tc.driverName, tc.source, "user:password2", create
+
+		handler, e := createServer(&driver, &source, &cred, &create)
+		assert.Nil(t, e)
+
+		req := test.NewRequest("PROPFIND", "/", strings.NewReader(body))
+		header := map[string][]string{
+			"Authorization": {authHeader},
+		}
+		req.Header = header
+		w := test.NewRecorder()
+		handler.ServeHTTP(w, req)
+
+		resp := w.Result()
+
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		fmt.Println(resp.StatusCode)
+		fmt.Println(resp.Header.Get("Content-Type"))
+		fmt.Println(string(body))
+		assert.Equal(t, 401, resp.StatusCode)
+	})
+}
+
+func TestGetCredSuccess(t *testing.T) {
+	runOnAllDrivers(t, func(tc testContext) {
+
+		driver, source, cred, create := tc.driverName, tc.source, "user:password", create
+
+		handler, e := createServer(&driver, &source, &cred, &create)
+		assert.Nil(t, e)
+
+		req := test.NewRequest("PROPFIND", "/", strings.NewReader(body))
+		header := map[string][]string{
+			"Authorization": {authHeader},
+		}
+		req.Header = header
+		w := test.NewRecorder()
+		handler.ServeHTTP(w, req)
+
+		resp := w.Result()
+
+		body, _ := ioutil.ReadAll(resp.Body)
+
+		fmt.Println(resp.StatusCode)
+		fmt.Println(resp.Header.Get("Content-Type"))
+		fmt.Println(string(body))
+		assert.Equal(t, 207, resp.StatusCode)
+	})
+}
+
+func TestClientCreateDir(t *testing.T) {
+	runOnAllDrivers(t, func(tc testContext) {
+		for i := 0; i < 100; i++ {
+			port := "9998"
+
+			user := "user"
+			password := "password"
+
+			driver, source, cred, create, addr := tc.driverName, tc.source, user+":"+password, create, ":"+port
+
+			handler, e := createServer(&driver, &source, &cred, &create)
+			assert.Nil(t, e)
+
+			ctx, cancel := context.WithCancel(context.Background())
+			srv := runServer(&addr, handler)
+
+			uri := "http://localhost:" + port
+
+			c := gowebdav.NewClient(uri, user, password)
+			_ = c.Connect() // performs authorization
+			err := c.Mkdir("folder", 0644)
+			assert.Nil(t, err, "Got error %v", err)
+
+			srv.Shutdown(ctx)
+			cancel()
+		}
+	})
 }
