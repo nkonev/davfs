012ba1efb54028060533a589c6340aa1d92e332d
Nikita Konev <nikit.cpp@yandex.ru>
2019-01-20 03:01:53 +0300

    rm useless test

--- cmd/davfs/davfs_test.go
+++ cmd/davfs/davfs_test.go
@@ -29,27 +29,6 @@ const (
 	authHeader = "Basic dXNlcjpwYXNzd29yZA=="
 )
 
-func TestGet(t *testing.T) {
-
-	driver, source, cred, create := driver, source, cred, create
-
-	handler, e := createServer(&driver, &source, &cred, &create)
-	assert.Nil(t, e)
-
-	req := test.NewRequest("GET", "/", nil)
-	w := test.NewRecorder()
-	handler.ServeHTTP(w, req)
-
-	resp := w.Result()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	fmt.Println(resp.StatusCode)
-	fmt.Println(resp.Header.Get("Content-Type"))
-	fmt.Println(string(body))
-	assert.Equal(t, 405, resp.StatusCode)
-}
-
 func TestGetCredFailedWithoutCredentials(t *testing.T) {
 
 	driver, source, cred, create := driver, source, "user:password", create
